8b968aa3bf6415ae6fb8c8619d5273f461013f8e
Lukas Rocha <contatolukasrocha@gmail.com>
2026-06-20 15:04:28 -0300

    fix: corrige apiUrl de produção para URL do Railway

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://SUBSTITUIR-PELA-URL-DO-RAILWAY.railway.app/api/v1'
+  apiUrl: 'https://tcc-bjj-fullsystem-production.up.railway.app/api/v1'
 };
